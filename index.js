fca50622b033403ea4ebad871db14c224beb9197
AZIZXlaouiti <azizrocke@gmail.com>
2021-06-08 21:23:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+ function saturdayFun(where="roller-skate"){
+    return `This Saturday, I want to ${where}!`
+}
+function mondayWork(where="go to the office"){
+     return`This Monday, I will ${where}.`
+}
+//"This Monday, I will go to the office."
+function wrapAdjective(sign = "*" )
+{
+    return function(name){
+        return `You are ${sign}${name}${sign}!`
+    }
+}
